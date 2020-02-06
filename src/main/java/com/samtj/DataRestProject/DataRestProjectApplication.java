51fa94d80d7495fc096ef3169616b8ac890a6470
Samuel Tijani <omalitijani@gmail.com>
2020-02-06 13:14:18 +0100

    Init commit

---
+++ src/main/java/com/samtj/DataRestProject/DataRestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.samtj.DataRestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samtj/DataRestProject/DataRestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samtj.DataRestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataRestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
